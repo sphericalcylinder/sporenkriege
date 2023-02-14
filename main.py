c7cfb5fec6ace40e3fa0452c900c2458cbe78647
sphericalcylinder <Jonahposner@icloud.com>
2023-02-14 16:09:12 -0500

    base code

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import pygame
+import json
+
+# Initialize pygame
+pygame.init()
+#pygame.font.init()
+
+# Set up drawing area/screen and define constants
+SCREEN = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Sporenkriege")
+pygame.event.set_allowed(None)
+
+CLOCK = pygame.time.Clock()
+
+def parse_settings():
+    with open('settings.json', 'r') as f:
+        return json.load(f)
+
+# Main function
+def main():
+
+    settings = parse_settings()
+
+    while True:
+        # Clear screen
+        SCREEN.fill(settings['bg'])
+
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+        # Draw stuff
+        # ...
+
+        # Update the screen
+        pygame.display.update()
+        # Set fps
+        CLOCK.tick(settings['fps'])
+
+
+if __name__ == '__main__':
+    main()
